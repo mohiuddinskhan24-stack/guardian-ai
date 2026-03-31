636a70181cfe4b6e88a3e14d686de8853f77982d
mohiuddinskhan24-stack <mohiuddin.s.khan24@slrtce.in>
2026-03-31 12:26:33 +0530

    Update github_integration.py

--- github_integration.py
+++ github_integration.py
@@ -5,15 +5,20 @@ g = Github(os.getenv("GITHUB_TOKEN"))
 
 def get_repo_files(repo_name):
     repo = g.get_repo(repo_name)
-    contents = repo.get_contents("")
 
     files = []
 
-    for content in contents:
-        if content.type == "file":
-            try:
-                files.append((content.name, content.decoded_content.decode()))
-            except:
-                pass
+    def get_all_files(contents):
+        for content in contents:
+            if content.type == "dir":
+                get_all_files(repo.get_contents(content.path))
+            elif content.type == "file":
+                try:
+                    files.append((content.name, content.decoded_content.decode()))
+                except:
+                    pass
+
+    contents = repo.get_contents("")
+    get_all_files(contents)
 
     return files
